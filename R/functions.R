296616141540f410bcdc6eebc4652036dceb521a
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2023-11-09 09:41:23 +0100

    new fun

--- R/functions.R
+++ R/functions.R
@@ -1,1 +1,7 @@
-r3::open_feedback_survey()
+descriptive_stats <- function(data) {
+  data |>
+    dplyr::group_by(metabolite) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+}
+
