2229b4b2db9626662c75b33eb49c902b69fc2355
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2023-11-09 10:31:42 +0100

    targets will change lives

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,9 @@
+#' Descriptive function
+#'
+#' @param data Liipidomics data set
+#'
+#' @return tibble with descriptive statistics
+#'
 descriptive_stats <- function(data) {
   data |>
     dplyr::group_by(metabolite) |>
@@ -5,3 +11,14 @@ descriptive_stats <- function(data) {
     dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
 
+
+#' Simple geom_histogram plot
+#'
+#' @param data Lipidomics data set
+#'
+#' @return ggplot object
+plot_distributions <- function(data){
+  data |> ggplot2::ggplot(ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(vars(metabolite), scales = "free")
+}
